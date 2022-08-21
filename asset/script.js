f230916f02a371d0a254cb79c581bedf1a40e5a0
Alok Singh Negi <aloknegi100@gmail.com>
2022-08-21 19:17:29 +0530

    calender and line through added

--- asset/script.js
+++ asset/script.js
@@ -1,8 +1,1 @@
-var check=document.getElementById('checkbox');
-var label=document.getElementById('label');
 
-// check.addEventListener('click',())
-if(check.checked===true)
-{
-    label.classList.add('clicked')
-}
