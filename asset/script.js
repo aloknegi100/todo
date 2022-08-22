44ec46d17b26b69b3792c272999cdf4c7f0d2f98
Alok Singh Negi <aloknegi100@gmail.com>
2022-08-22 14:13:56 +0530

    completed

--- asset/script.js
+++ asset/script.js
@@ -1,1 +1,19 @@
+let button=document.getElementById('delete-task');
+button.addEventListener('click',()=>{
+    var checkbox=document.querySelectorAll('.checkbox');
+    var arr=[];
+    for(i of checkbox)
+    {
+        if(i.checked)
+        {
+            arr.push(i.dataset.id);
+        }
+    }
+    let url="/delete/?id=";
+    for(i of arr)
+    {
+        url+=i+"%";
+    }
+    window.location=url;
 
+})
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,28 @@ const express=require('express');
 const router=express.Router();
 const homeController=require('../controllers/home_Controller');
 const addController=require('../controllers/add_Controller')
+const Task=require('../models/to-do-list')
 
 router.get('/',homeController.home);
 
 router.post('/add',addController.add);
+router.get('/delete',(req,res)=>{
+    let string=req.query.id;
+    let arr=string.split("%");
+    arr.pop();
+    console.log(arr);
+    for(i of arr)
+    {
+        Task.findByIdAndDelete(i,(err)=>{
+            if(err)
+            {
+                console.log('error in deleting from database');
+            }
+        });
+       
+    }
+    res.redirect('/');
+})
 console.log('router loaded');
 
 module.exports=router;
